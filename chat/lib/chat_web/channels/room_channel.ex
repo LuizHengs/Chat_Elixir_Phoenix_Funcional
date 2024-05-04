e8a37229472bb88a4461e6180e26ce7357e21433
LuizHengs <luizhengs@gmail.com>
2024-05-04 17:34:35 -0300

    Parece funcional

--- chat/lib/chat_web/channels/room_channel.ex
+++ chat/lib/chat_web/channels/room_channel.ex
@@ -1,9 +1,9 @@
 defmodule ChatWeb.RoomChannel do
   use ChatWeb, :channel
 
-  @impl true
   def join("room:lobby", payload, socket) do
     if authorized?(payload) do
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -21,6 +21,7 @@ defmodule ChatWeb.RoomChannel do
   # broadcast to everyone in the current topic (room:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
+    Chat.Message.changeset(%Chat.Message{}, payload) |> Chat.Repo.insert
     broadcast(socket, "shout", payload)
     {:noreply, socket}
   end
@@ -29,4 +30,19 @@ defmodule ChatWeb.RoomChannel do
   defp authorized?(_payload) do
     true
   end
+
+  @impl true
+  def handle_info(:after_join, socket) do
+    Chat.Message.get_messages()
+    |> Enum.reverse() # revers to display the latest message at the bottom of the page
+    |> Enum.each(fn msg -> push(socket, "shout", %{
+        name: msg.name,
+        message: msg.message,
+        inserted_at: msg.inserted_at,
+      }) end)
+    {:noreply, socket} # :noreply
+  end
+
+
+
 end
